c1799383ba16b0c3a8dc8b0d180021819e42360e
Evan Liu <qingyan123@gmail.com>
2012-04-24 02:21:17 +0800

    implement updateTimerHandler to update UI timer.

--- TomatoClock/src/info/liuqy/adc/tomatoclock/TomatoClockActivity.java
+++ TomatoClock/src/info/liuqy/adc/tomatoclock/TomatoClockActivity.java
@@ -3,15 +3,32 @@ package info.liuqy.adc.tomatoclock;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class TomatoClockActivity extends Activity {
 	static final int VIBRATE_ACTION = 0;
     public static final long TWENTYFIVE_MIN = 5*1000; //only for class demo. real: 25*60*1000;
 
+	private TimerUpdateService srv = null;
+	private ServiceConnection conn = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder binder) {
+			srv = ((TimerUpdateService.MyBinder)binder).getService();
+		}
+		public void onServiceDisconnected(ComponentName className) {
+			srv = null;
+		}
+	};
+   
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +36,24 @@ public class TomatoClockActivity extends Activity {
         setContentView(R.layout.main);
     }
     
+    private Handler updateTimerHandler = new Handler() {
+
+		/* (non-Javadoc)
+		 * @see android.os.Handler#handleMessage(android.os.Message)
+		 */
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			Button clockBtn = (Button)TomatoClockActivity.this.findViewById(R.id.start_timer);
+			//int secs = (Integer)msg.obj;
+			long secs = srv.getTimeElapsed();
+			int min = (int) secs/60;
+			int sec = (int) secs - min*60;
+			clockBtn.setText(String.format("%1$02d:%2$02d", min, sec));
+		}
+    	
+    };
+
     public void startTimer(View v) {
         Intent i = new Intent(this, TomatoVibrator.class);
         PendingIntent pi = PendingIntent.getBroadcast(
@@ -26,5 +61,8 @@ public class TomatoClockActivity extends Activity {
         AlarmManager am = (AlarmManager)this.getSystemService(ALARM_SERVICE);
         am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + TWENTYFIVE_MIN, pi);
         Toast.makeText(this, "TomatoTimer started!", Toast.LENGTH_SHORT).show();      
+
+        Intent service = new Intent(this, TimerUpdateService.class);
+        bindService(service, conn, Context.BIND_AUTO_CREATE);
     }
 }
