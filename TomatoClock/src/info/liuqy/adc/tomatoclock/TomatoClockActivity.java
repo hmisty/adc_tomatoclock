85ed8ef47d99af66d90596896cfc00d04d853916
Evan Liu <qingyan123@gmail.com>
2012-04-24 01:57:24 +0800

    use PendingIntent & AlarmManager to send delayed broadcast. DEMO!

--- TomatoClock/src/info/liuqy/adc/tomatoclock/TomatoClockActivity.java
+++ TomatoClock/src/info/liuqy/adc/tomatoclock/TomatoClockActivity.java
@@ -1,11 +1,17 @@
 package info.liuqy.adc.tomatoclock;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class TomatoClockActivity extends Activity {
+	static final int VIBRATE_ACTION = 0;
+    public static final long TWENTYFIVE_MIN = 5*1000; //only for class demo. real: 25*60*1000;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,10 @@ public class TomatoClockActivity extends Activity {
     
     public void startTimer(View v) {
         Intent i = new Intent(this, TomatoVibrator.class);
-        this.sendBroadcast(i);
+        PendingIntent pi = PendingIntent.getBroadcast(
+                this.getApplicationContext(), VIBRATE_ACTION, i, 0);
+        AlarmManager am = (AlarmManager)this.getSystemService(ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + TWENTYFIVE_MIN, pi);
+        Toast.makeText(this, "TomatoTimer started!", Toast.LENGTH_SHORT).show();      
     }
 }
