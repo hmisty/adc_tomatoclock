52a3a190ebfbf026109303b561c7fc4c50b49e23
Evan Liu <qingyan123@gmail.com>
2012-04-24 02:20:19 +0800

    add getter for timeElapsed.

--- TomatoClock/src/info/liuqy/adc/tomatoclock/TimerUpdateService.java
+++ TomatoClock/src/info/liuqy/adc/tomatoclock/TimerUpdateService.java
@@ -15,7 +15,11 @@ public class TimerUpdateService extends Service {
     private static final long UPDATE_INTERVAL = 1000; //1 sec
     private long timeElapsed = 0;
 
-    /*FIXME really bad practice
+    public long getTimeElapsed() {
+		return timeElapsed;
+	}
+
+	/*FIXME really bad practice
      * see here: http://www.ozdroid.com/#!BLOG/2010/12/19/How_to_make_a_local_Service_and_bind_to_it_in_Android
      */
     private final IBinder mBinder = new MyBinder();
