a46a9429e9b3dfeacdea43de4625792c006b3a70
Evan Liu <qingyan123@gmail.com>
2012-04-24 01:35:42 +0800

    TomatoService skeleton

---
+++ TomatoClock/src/info/liuqy/adc/tomatoclock/TomatoTimerService.java
@@ -0,0 +1,19 @@
+package info.liuqy.adc.tomatoclock;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class TomatoTimerService extends IntentService {
+
+	public TomatoTimerService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
