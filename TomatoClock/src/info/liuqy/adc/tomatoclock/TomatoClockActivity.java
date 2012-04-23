eaff0ab81358b1e1c30b126c99d79f6b4f9ea2ad
Evan Liu <qingyan123@gmail.com>
2012-04-24 01:43:05 +0800

    implement startTimer button & callback.

--- TomatoClock/src/info/liuqy/adc/tomatoclock/TomatoClockActivity.java
+++ TomatoClock/src/info/liuqy/adc/tomatoclock/TomatoClockActivity.java
@@ -1,7 +1,9 @@
 package info.liuqy.adc.tomatoclock;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TomatoClockActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,10 @@ public class TomatoClockActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void startTimer(View v) {
+        Intent service = new Intent(TomatoClockActivity.this, TomatoTimerService.class);
+        TomatoClockActivity.this.startService(service);
     }
 }
