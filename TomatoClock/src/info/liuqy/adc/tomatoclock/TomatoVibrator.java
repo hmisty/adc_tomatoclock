c43d489956c734b67e27e046e91e9c722a395f53
Evan Liu <qingyan123@gmail.com>
2012-04-24 01:49:49 +0800

    TomatoVibrator skeleton.

---
+++ TomatoClock/src/info/liuqy/adc/tomatoclock/TomatoVibrator.java
@@ -0,0 +1,15 @@
+package info.liuqy.adc.tomatoclock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class TomatoVibrator extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
