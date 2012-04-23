81ccb038f0074fa1a18fcd0dec46814faa9949ca
Evan Liu <qingyan123@gmail.com>
2012-04-24 01:50:37 +0800

    implement TomatoVibrator.onReceive.

--- TomatoClock/src/info/liuqy/adc/tomatoclock/TomatoVibrator.java
+++ TomatoClock/src/info/liuqy/adc/tomatoclock/TomatoVibrator.java
@@ -3,13 +3,16 @@ package info.liuqy.adc.tomatoclock;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
 
 public class TomatoVibrator extends BroadcastReceiver {
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
-		// TODO Auto-generated method stub
-
+	public void onReceive(Context ctx, Intent intent) {
+        Toast.makeText(ctx, "TomatoTimer timed out!", Toast.LENGTH_SHORT).show();
+        Vibrator vib = (Vibrator)ctx.getSystemService(Context.VIBRATOR_SERVICE);
+        vib.vibrate(2000); //FIXME magic number
 	}
 
 }
